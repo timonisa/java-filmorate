2e98bdfb5fcc985030f6df6eb607458eac167104
timonissa <dzenn2@yandex.ru>
2023-04-03 11:38:54 +0300

    filmorate 0.1

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -4,6 +4,7 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,11 +23,12 @@ public class FilmController {
     }
 
     @PostMapping(value = "/films")
-    public Film create(@RequestBody Film film){
-        if (validator(film)){
-            for (int i = 0; i < films.size(); i++){
+    public Film create(@RequestBody Film film) {
+        if (validator(film)) {
+            for (int i = 0; i < films.size(); i++) {
                 boolean isExist = films.get(i).equals(film);
-                if(isExist) throw new UserAlreadyExistException();;
+                if (isExist) throw new UserAlreadyExistException();
+                ;
                 log.info("Уже есть такой фильм " + film.toString());
             }
             film.setId(nextId);
@@ -42,23 +44,23 @@ public class FilmController {
 
     @PutMapping(value = "/films")
     public Film update(@RequestBody Film film) throws ValidationException {
-        if(validator(film)){
-                if(films.get(film.getId()-1) != null){
-                    films.set(film.getId()-1, film);
-                    log.info("Обновлен кинчик " + film.toString());
-                    return film;
-                }else{
-                    log.error("не найден фильм " + film.toString());
-                    throw new ValidationException("не найден фильм");
-                }
-        }else{
+        if (validator(film)) {
+            if (films.get(film.getId() - 1) != null) {
+                films.set(film.getId() - 1, film);
+                log.info("Обновлен кинчик " + film.toString());
+                return film;
+            } else {
+                log.error("не найден фильм " + film.toString());
+                throw new ValidationException("не найден фильм");
+            }
+        } else {
             log.error("не прошел валидацию фильм  " + film.toString());
             throw new ValidationException("не прошел валидацию");
         }
     }
 
-    public boolean validator (Film film){
-        return !film.getName().isEmpty() && film.getDescription().length()<201 && film.getReleaseDate().isAfter(firstMovieDate) &&
-                film.getDuration()>0;
+    public boolean validator(Film film) {
+        return !film.getName().isEmpty() && film.getDescription().length() < 201 && film.getReleaseDate().isAfter(firstMovieDate) &&
+                film.getDuration() > 0;
     }
 }
